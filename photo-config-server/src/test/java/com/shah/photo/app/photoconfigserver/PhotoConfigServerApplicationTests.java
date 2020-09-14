ac24b9d7d841090975785d40410ee0a2b3294a6c
Milan Shah <mshah1992@live.com>
2020-09-14 17:47:01 -0400

    Added spring cloud config server to share bootstrap.properties

---
+++ photo-config-server/src/test/java/com/shah/photo/app/photoconfigserver/PhotoConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shah.photo.app.photoconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
